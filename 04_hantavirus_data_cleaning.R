4cf5257592d422f114e71d2b7681e467820ddebc
Janine Mistrick <jmistrick@gmail.com>
2024-11-11 14:24:13 -0600

    updated problemchildren and netmets_puuv for pct_overlap + 2023 data
    THERE ARE STILL SOME SUMMARY PIECES TO RERUN FOR 2021-2023 - sorry, didn't feel like doing it rn

--- 04_hantavirus_data_cleaning.R
+++ 04_hantavirus_data_cleaning.R
@@ -31,31 +31,33 @@ rm(list = ls())
 ### Fulltrap and netmets dfs created separately, load 2021 and 2022 data here, clean as needed
 
 ### LOAD INDIVIDUAL VOLE METADATA by year 
-fulltrap21 <- readRDS(file="fulltrap21_03.04.24.rds")
-fulltrap22 <- readRDS(file="fulltrap22_03.04.24.rds")
+fulltrap21 <- readRDS(file="fulltrap21_11.11.24.rds")
+fulltrap22 <- readRDS(file="fulltrap22_11.11.24.rds")
+fulltrap23 <- readRDS(file="fulltrap23_11.11.24.rds")
 
-## >>NOTE<< overwintered animals (caught in 2021 and 2022) may incorrectly have firstcap==1 in 2022
+## >>NOTE<< overwintered animals may incorrectly have firstcap==1 in 2022 or 2023
     # PIT tag: 219895 Occ2Sess1 at Vaarinkorpi 2022
     # PIT tag: 226280 Occ2Sess2 at Kuoppa 2022
 
-#join 21 and 22 fulltrap data; correct firstcap for 2022
-fulltrap21.22 <- rbind(fulltrap21, fulltrap22) %>%
+#join 21, 22, 23 fulltrap data; correct firstcap for 2022 and 2023
+fulltrapALL <- rbind(fulltrap21, fulltrap22, fulltrap23) %>%
   group_by(tag) %>% arrange(tag, year, occ.sess, .by_group = TRUE) %>%
   mutate(firstcap = ifelse(date_time == min(date_time), 1, 0)) %>%
   mutate(firstcap = factor(firstcap)) %>% ungroup() %>%
 #keep one entry per tag,year,month; pull only relevant columns
   group_by(tag, year, month) %>% slice(1) %>%
-  select(year, site, trt, month, tag, samp_id, sex, season_breeder, traps_per_life, caps_per_life) %>%
+  select(year, site, trt, month, tag, samp_id, sex, season_breeder, traps_per_year, caps_per_year) %>%
   ungroup()
 
 ### LOAD NETWORK METRICS data
 netmets21 <- readRDS(file="pctover_netmets21.rds") 
 netmets22 <- readRDS(file="pctover_netmets22.rds") 
+netmets23 <- readRDS(file="pctover_netmets23.rds") 
 #join 2021 and 2022
-netmets21.22 <- rbind(netmets21, netmets22)
+netmetsALL <- rbind(netmets21, netmets22, netmets23)
 
 ### JOIN NETWORK METRICS + METADATA
-netmets_full <- left_join(netmets21.22, fulltrap21.22, by=c("year", "site", "month", "tag")) %>%
+netmets_full <- left_join(netmetsALL, fulltrapALL, by=c("year", "site", "month", "tag")) %>%
   mutate(site=as.factor(site),
          year=as.factor(year),
          month = as.factor(month),
@@ -63,16 +65,16 @@ netmets_full <- left_join(netmets21.22, fulltrap21.22, by=c("year", "site", "mon
   select(!c(focal_id)) %>% #remove duplicate column for PIT tag number
   relocate(c(year, trt, site, month, n.node, tag, samp_id, sex, season_breeder), .before = wt.deg.in)
 
-saveRDS(netmets_full, here("netmets_full_11.07.24.rds"))
+saveRDS(netmets_full, here("netmets_full_11.11.24.rds"))
 
 # #load the most recent version of netmets_full
-# netmets_full <- readRDS(here("netmets_full_11.07.24.rds"))
+# netmets_full <- readRDS(here("netmets_full_11.11.24.rds"))
 
 
 #########################################   LOAD & CLEAN PUUV IFA DATA   ########################################
 
 # #load PUUV IFA data
-# puuv_data <- read.csv(here("puuv_ifa_06.07.23.csv")) %>%
+# puuv_data <- read.csv(here("puuv_ifa_11.11.24.csv")) %>%
 #   clean_names %>%
 #   #populate column of FINAL PUUV status
 #   #kind of a pain, since samples could be run 1-4x but we want the result of the last run as the 'final' status
@@ -91,15 +93,15 @@ saveRDS(netmets_full, here("netmets_full_11.07.24.rds"))
 #          # puuv_run4 = as.factor(puuv_run4),
 #          FINAL_puuv = as.factor(FINAL_puuv)) %>%
 #   drop_na(FINAL_puuv) %>%
-#   dplyr::select(FINAL_puuv, samp_id) %>%
+#   dplyr::select(samp_id, FINAL_puuv) %>%
 #   rename(puuv_ifa = FINAL_puuv)
 # #output is a df with two columns, sample ID and PUUV status (0,1)
 # 
 # # Save puuv_data to a rdata file
-# saveRDS(puuv_data, file = here("hantadata_06.09.23.rds"))
+# saveRDS(puuv_data, file = here("hantadata_11.11.24.rds"))
 
 # Restore puuv_data from the rdata file
-puuv_data <- readRDS(file = "hantadata_06.09.23.rds")
+puuv_data <- readRDS(file = "hantadata_11.11.24.rds")
 
 
 ##################################  COMBINE NETMETS data with PUUV data ######################################
@@ -111,10 +113,15 @@ netmets_puuv <- left_join(netmets_full, puuv_data, by="samp_id") %>%
   select(!samp_id) %>%
   drop_na(puuv_ifa) #drop any animals without puuv data
 
+
+
+### NEW!!! ### NOV 2024 UPDATE - there are 138 that drop out, so 18 new ones with 2023 data
+
 ########## NOTE: the step above DROPS voles with no PUUV infection status data
 ## there are 120 entries that we have network data for a vole but DO NOT know their PUUV status
 ## these voles ARE in the networks since we know they were there and where they were, 
 ## but we can't use for models of how spatial overlap impacts infection status
+
 
 
 ############ REMOVE THE VOLES from netmets_puuv THAT SEROCONVERT POS TO NEG ##################
@@ -129,12 +136,13 @@ puuv_pos_neg <- netmets_puuv %>% group_by(tag) %>% arrange(year, month, .by_grou
   dplyr::select(year, month, tag, puuv_ifa) %>%
   summarise(status_time = toString(puuv_ifa)) %>%
   filter(str_detect(status_time, "1,\\s0")) #filter for animals that go from pos to neg
-#pull the PIT tags (21 individuals (50 entries), mostly 1-0, a few 1-0-1)
+#pull the PIT tags (29 individuals (72 entries), mostly 1-0, a few 1-0-1)
 problemchildren <- puuv_pos_neg$tag
 #filter netmetsPUUV to remove 'problemchildren' - status is inconclusive or maybe we detected MatAb
 netmets_puuv <- netmets_puuv %>%
   filter(!tag %in% problemchildren)
 
+###################### FOLLOWING HAS NOT BEEN UPDATED WITH 2023 DATA ###########################################
 ####################### this probably doesn't need to be published #############################################
 ###### BRIEFLY, who are the voles that convert PUUV + to - ? #######
 # #pull the puuv data on "problem children"
@@ -170,7 +178,7 @@ netmets_puuv <- netmets_puuv %>%
 
 ######################### end problem children ################################
 
-
+############ HAS NOT BEEN UPDATED WITH 2023 DATA #################
 ## BEFORE GOING ON - please note:
 ## there are 170 entries from netmets21 and netmets22 that DO NOT make it in to netmets_PUUV
 ## these are either animals without PUUV IFA data (120 entries) or animals with inconsistent IFA results (50 entries, 21 voles)
@@ -187,6 +195,7 @@ netmets_puuv <- netmets_puuv %>%
 
 
 
+############ HAS NOT BEEN UPDATED WITH 2023 DATA #################
 
 # ####################################################################
 # ####################################################################
@@ -221,7 +230,7 @@ netmets_puuv <- netmets_puuv %>%
 
 
 
-
+############ HAS NOT BEEN UPDATED WITH 2023 DATA #################
 
 # ###############################################################################
 # ### exploratory behavior of voles (breeders/non-breeders) ###
@@ -287,7 +296,7 @@ netmets_puuv <- netmets_puuv %>%
 ######### lagged degree and seroconvert status ###############
 # add previous (lagged) degree (degree from previous month influences current PUUV status)
 # add 0,1 for serovert - animals that go 0-0 or 0-1
-    # BUT! the previous month has to be in the same year (don't want 2021 fall to influence 2022 spring)
+    # BUT! the previous month has to be in the same year (eg don't want 2021 fall to influence 2022 spring)
 netmets_puuv <- netmets_puuv %>% group_by(year, tag) %>%
   arrange(month, .by_group = TRUE) %>%
   mutate(prev_wt.deg.in = lag(wt.deg.in, n=1),
@@ -310,79 +319,80 @@ netmets_puuv <- netmets_puuv %>% group_by(year, tag) %>%
 
 
 #save all the above code to here
-saveRDS(netmets_puuv, here("netmets_puuv_11.07.24.rds"))
+saveRDS(netmets_puuv, here("netmets_puuv_11.11.24.rds"))
 
 # #load netmets_puuv
-# netmets_puuv <- readRDS(here("netmets_puuv_11.07.24.rds"))
+# netmets_puuv <- readRDS(here("netmets_puuv_11.11.24.rds"))
 
 
 
+############ HAS NOT BEEN UPDATED WITH 2023 DATA #################
 
-##################################################################################################################
-
-### Additional Summarized Data to Report in Manuscript ###
-
-##TOTAL number of animals captured, recapped etc in 2021 and 2022 (MAY-OCTOBER)
-  # before detailing the subset of the data used for this specific study
-
-## so that would be the fulltrap_ALL_03.04.24 dataset (see 01_...data_cleaning.R)
-#INCLUDING MAY captures, INCLUDING sex/repro=NA
-fulltrap21_ALL <- readRDS(file="fulltrap21_ALL_03.04.24.rds")
-fulltrap22_ALL <- readRDS(file="fulltrap22_ALL_03.04.24.rds")
-fulltrap21.22_ALL <- rbind(fulltrap21_ALL, fulltrap22_ALL)
-#4286 capture events (including WR) across MAY-Oct 2021 and 2022
-n_distinct(fulltrap21.22_ALL$tag) #1518 unique voles (including MAY)
-recapped <- fulltrap21.22_ALL %>% filter(caps_per_life>1)
-n_distinct(recapped$tag) #929 voles were recapped at least once
-## number captured in both years (how many overwintered 2021->2022)
-ow <- fulltrap21.22_ALL %>% group_by(year, tag) %>% slice(1) %>% ungroup() %>%
-  group_by(tag) %>% mutate(n=length(tag)) %>% filter(n>1) %>% arrange(tag)
-n_distinct(ow$tag) #12 voles in both 2021 and 2022
-
-##-----------------
-
-##DATA USED FOR VARIOUS STEPS OF THE ANALYSIS IN THE MANUSCRIPT:
-
-#entries per year (in 'netmets' files --> i.e., those used to construct the spatial overlap networks) 
-  # these entries may or may not have PUUV data
-nrow(netmets21) #1129 entries in 2021
-n_distinct(netmets21$tag) #742 voles in 2021
-nrow(netmets22) #1131 entries in 2022
-n_distinct(netmets22$tag) #744 voles in 2022
-
-n_distinct(netmets21.22$tag) #1476 unique voles in the networks, 10 are in both 2021 and 2022
-
-## "Spatial overlap networks were constructed with all voles captured in June-October with recorded sex 
-## and reproductive status data: 1129 captures (742 unique voles) in 2021 and 1131 captures (744 unique voles) in 2022."
-
-## THIS IS NO LONGER INCLUDED IN THE MANUSCRIPT:
-# #entries per year (in netmets_puuv --> i.e., those that do have PUUV data)
-#   # i.e., the above counts minus the 170 entries removed for lacking or inconsistent PUUV data
-# y1 <- netmets_puuv %>% filter(year=="2021") #1029 in 2021
-# n_distinct(y1$tag) #683 unique voles
-# y2 <- netmets_puuv %>% filter(year=="2022") #1061 in 2022
-# n_distinct(y2$tag) #694 unique voles
+# ##################################################################################################################
 # 
-# ## "Of these, we obtained consistent hantavirus serology results for 1,029 captures (683 unique voles) in June-October 
-# ## 2021 and 1,061 captures (694 unique voles) in June-October 2022."
-
-##---------------
-
-#one last thing...
-#how many times is the "previous network position" (used as the explanatory variable in the hanta probability modesl)
-  #NOT from the immediately prior month?
-
-df <- netmets_puuv %>% group_by(tag) %>% mutate(month.n = case_when(month=="june" ~ 1,
-                                                              month=="july" ~ 2,
-                                                              month=="aug" ~ 3,
-                                                              month=="sept" ~ 4,
-                                                              month=="oct" ~ 5)) %>%
-  mutate(dif = month.n - lag(month.n)) %>%
-  drop_na(prev_wt.deg)
-#713 data entries with a current PUUV informed by previous network position
-
-dfsmol <- df %>% filter(dif > 1) 
-#25 times we had to pull from an earlier month that wasn't the previous one (but was still in that calendar year)
-#25/713 = 3.5%
-
-############################################ The End #############################################
+# ### Additional Summarized Data to Report in Manuscript ###
+# 
+# ##TOTAL number of animals captured, recapped etc in 2021 and 2022 (MAY-OCTOBER)
+#   # before detailing the subset of the data used for this specific study
+# 
+# ## so that would be the fulltrap_ALL_03.04.24 dataset (see 01_...data_cleaning.R)
+# #INCLUDING MAY captures, INCLUDING sex/repro=NA
+# fulltrap21_ALL <- readRDS(file="fulltrap21_ALL_03.04.24.rds")
+# fulltrap22_ALL <- readRDS(file="fulltrap22_ALL_03.04.24.rds")
+# fulltrap21.22_ALL <- rbind(fulltrap21_ALL, fulltrap22_ALL)
+# #4286 capture events (including WR) across MAY-Oct 2021 and 2022
+# n_distinct(fulltrap21.22_ALL$tag) #1518 unique voles (including MAY)
+# recapped <- fulltrap21.22_ALL %>% filter(caps_per_life>1)
+# n_distinct(recapped$tag) #929 voles were recapped at least once
+# ## number captured in both years (how many overwintered 2021->2022)
+# ow <- fulltrap21.22_ALL %>% group_by(year, tag) %>% slice(1) %>% ungroup() %>%
+#   group_by(tag) %>% mutate(n=length(tag)) %>% filter(n>1) %>% arrange(tag)
+# n_distinct(ow$tag) #12 voles in both 2021 and 2022
+# 
+# ##-----------------
+# 
+# ##DATA USED FOR VARIOUS STEPS OF THE ANALYSIS IN THE MANUSCRIPT:
+# 
+# #entries per year (in 'netmets' files --> i.e., those used to construct the spatial overlap networks) 
+#   # these entries may or may not have PUUV data
+# nrow(netmets21) #1129 entries in 2021
+# n_distinct(netmets21$tag) #742 voles in 2021
+# nrow(netmets22) #1131 entries in 2022
+# n_distinct(netmets22$tag) #744 voles in 2022
+# 
+# n_distinct(netmets21.22$tag) #1476 unique voles in the networks, 10 are in both 2021 and 2022
+# 
+# ## "Spatial overlap networks were constructed with all voles captured in June-October with recorded sex 
+# ## and reproductive status data: 1129 captures (742 unique voles) in 2021 and 1131 captures (744 unique voles) in 2022."
+# 
+# ## THIS IS NO LONGER INCLUDED IN THE MANUSCRIPT:
+# # #entries per year (in netmets_puuv --> i.e., those that do have PUUV data)
+# #   # i.e., the above counts minus the 170 entries removed for lacking or inconsistent PUUV data
+# # y1 <- netmets_puuv %>% filter(year=="2021") #1029 in 2021
+# # n_distinct(y1$tag) #683 unique voles
+# # y2 <- netmets_puuv %>% filter(year=="2022") #1061 in 2022
+# # n_distinct(y2$tag) #694 unique voles
+# # 
+# # ## "Of these, we obtained consistent hantavirus serology results for 1,029 captures (683 unique voles) in June-October 
+# # ## 2021 and 1,061 captures (694 unique voles) in June-October 2022."
+# 
+# ##---------------
+# 
+# #one last thing...
+# #how many times is the "previous network position" (used as the explanatory variable in the hanta probability modesl)
+#   #NOT from the immediately prior month?
+# 
+# df <- netmets_puuv %>% group_by(tag) %>% mutate(month.n = case_when(month=="june" ~ 1,
+#                                                               month=="july" ~ 2,
+#                                                               month=="aug" ~ 3,
+#                                                               month=="sept" ~ 4,
+#                                                               month=="oct" ~ 5)) %>%
+#   mutate(dif = month.n - lag(month.n)) %>%
+#   drop_na(prev_wt.deg)
+# #713 data entries with a current PUUV informed by previous network position
+# 
+# dfsmol <- df %>% filter(dif > 1) 
+# #25 times we had to pull from an earlier month that wasn't the previous one (but was still in that calendar year)
+# #25/713 = 3.5%
+# 
+# ############################################ The End #############################################
