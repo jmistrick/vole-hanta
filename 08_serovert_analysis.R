0607fea94828386b68e2e7ea8910ecde84e0307e
Janine Mistrick <jmistrick@gmail.com>
2024-11-12 11:48:39 -0600

    summarize previous degree measures for nonverts and compare to seroverts (visually)

--- 08_serovert_analysis.R
+++ 08_serovert_analysis.R
@@ -21,22 +21,27 @@ netmets_puuv <- readRDS(here("netmets_puuv_11.11.24.rds"))
 
 ##-------------------------------------------
 
-# #subset for voles that don't serovert (but do have previous capture)
-# novert <- netmets_puuv %>% filter(serovert==0) %>%
-#   select(!c(wt.deg.in, bin.in.deg, F.deg, M.deg, b.deg, nb.deg, mnb.deg, fnb.deg, mb.deg, fb.deg))
-# 
-# 
-# 
-# #subset to only voles that seroconvert
-# serovert <- netmets_puuv %>% filter(serovert==1) %>%
-#   select(!c(wt.deg.in, bin.in.deg, F.deg, M.deg, b.deg, nb.deg, mnb.deg, fnb.deg, mb.deg, fb.deg))
-# 
-# #plot previous degree for seroverts
-# serovert %>% ggplot(aes(x=prev_wt.deg.in, fill=trt)) +
-#   geom_histogram() +
-#   facet_wrap(~trt)
-# 
-# #lol there are so many voles with 0 overlaps in the previous month that seroconvert *dies*
+#subset for voles that don't serovert (but do have previous capture)
+nonvert <- netmets_puuv %>% filter(serovert==0) %>%
+  select(!c(wt.deg.in, bin.in.deg, F.deg, M.deg, b.deg, nb.deg, mnb.deg, fnb.deg, mb.deg, fb.deg))
+#655 observations 
+n_distinct(nonvert$tag) #490 individuals
+
+
+#subset to only voles that seroconvert
+serovert <- netmets_puuv %>% filter(serovert==1) %>%
+  select(!c(wt.deg.in, bin.in.deg, F.deg, M.deg, b.deg, nb.deg, mnb.deg, fnb.deg, mb.deg, fb.deg))
+#82 observations (individuals)
+
+
+###----------------- Visualize some stuff ------------------
+
+#plot previous degree for seroverts
+serovert %>% ggplot(aes(x=prev_wt.deg.in, fill=trt)) +
+  geom_histogram() +
+  facet_wrap(~trt)
+
+#lol there are so many voles with 0 overlaps in the previous month that seroconvert *dies*
 
 
 dat <- netmets_puuv %>% drop_na(serovert)
@@ -96,7 +101,6 @@ dat %>%
                geom = "crossbar", width = 0.5, lwd = 0.4)
 
 
-
 # #for wanelik/farine overlaps
 # dat %>% 
 #   filter(prev_wt.deg>0) %>% #remove voles with 0 overlaps in previous month
@@ -114,3 +118,69 @@ dat %>%
   geom_beeswarm() +
   stat_summary(aes(group = serovert, color=serovert), fun = mean, fun.min = mean, fun.max = mean,
                geom = "crossbar", width = 0.5, lwd = 0.4)
+
+
+######--------------------------------------------------------------
+
+#standard error (std dev of a population)
+SE <- function(x) sd(x)/sqrt(length(x))
+
+nonvert_means.month <- nonvert %>% 
+  group_by(year, trt, month) %>%
+  summarise(prev.in = mean(prev_wt.deg.in), se.prev.in = SE(prev_wt.deg.in),
+            prev.M = mean(prev_M.deg), se.prev.M = SE(prev_M.deg),
+            prev.b = mean(prev_b.deg), se.prev.b = SE(prev_b.deg),
+            prev.mb = mean(prev_mb.deg), se.prev.mb = SE(prev_mb.deg),
+            serovert = factor(0, levels=c("0", "1")))
+
+plotdata <- serovert %>% select(c(year, trt, month, site, tag, sex, season_breeder, 
+                                  prev_wt.deg.in, prev_M.deg, prev_mb.deg, prev_b.deg, serovert)) %>%
+  rename(prev.in = prev_wt.deg.in,
+         prev.M = prev_M.deg,
+         prev.b = prev_b.deg,
+         prev.mb = prev_mb.deg) %>%
+  rbind(nonvert_means.month) 
+
+
+plotdata %>% ggplot(aes(x=month)) +
+  geom_point(aes(y=prev.in, color=serovert, shape=serovert),
+             position = position_jitterdodge(0.2, dodge.width = .2)) +
+  geom_errorbar(aes( ymin = (prev.in-se.prev.in), 
+                     ymax = (prev.in + se.prev.in))) +
+  scale_shape_manual(name = "Sero Status", values=c(1,4)) +
+  scale_colour_manual(name = "Sero Status", values=c("black", "red")) +
+  labs(title="Previous Weighted In Degree") +
+  facet_grid(year ~ trt)
+
+plotdata %>% ggplot(aes(x=month)) +
+  geom_point(aes(y=prev.M, color=serovert, shape=serovert),
+             position = position_jitterdodge(0.2, dodge.width = .2)) +
+  geom_errorbar(aes( ymin = (prev.M-se.prev.M), 
+                     ymax = (prev.M + se.prev.M))) +
+  scale_shape_manual(name = "Sero Status", values=c(1,4)) +
+  scale_colour_manual(name = "Sero Status", values=c("black", "red")) +
+  labs(title="Previous Male Degree") +
+  facet_grid(year ~ trt)
+
+plotdata %>% ggplot(aes(x=month)) +
+  geom_point(aes(y=prev.b, color=serovert, shape=serovert),
+             position = position_jitterdodge(0.2, dodge.width = .2)) +
+  geom_errorbar(aes( ymin = (prev.b-se.prev.b), 
+                     ymax = (prev.b + se.prev.b))) +
+  scale_shape_manual(name = "Sero Status", values=c(1,4)) +
+  scale_colour_manual(name = "Sero Status", values=c("black", "red")) +
+  labs(title="Previous Breeder Degree") +
+  facet_grid(year ~ trt)
+
+plotdata %>% ggplot(aes(x=month)) +
+  geom_point(aes(y=prev.mb, color=serovert, shape=serovert),
+             position = position_jitterdodge(0.2, dodge.width = .2)) +
+  geom_errorbar(aes( ymin = (prev.mb-se.prev.mb), 
+                     ymax = (prev.mb + se.prev.mb))) +
+  scale_shape_manual(name = "Sero Status", values=c(1,4)) +
+  scale_colour_manual(name = "Sero Status", values=c("black", "red")) +
+  labs(title="Previous Male Breeder Degree") +
+  facet_grid(year ~ trt)
+
+
+
